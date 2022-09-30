8eff80affa1f8841f69c92c87fbf69731299a577
Michael <969149642@qq.com>
2022-09-30 16:13:16 +0800

    :sparkles: feat: 新增Button组件

---
+++ pages/Components/components/Button/Button.vue
@@ -0,0 +1,196 @@
+<template>
+	<button 
+    class="c-button-warp m" 
+    :class="[size, type,{'plain':plain,'disabled':disabled}]" 
+    :style="innerCustomStyle" 
+    :disabled="disabled"
+    :open-type="openType" 
+    @tap.stop="handleClick" 
+    @getphonenumber="getPhoneNumber" 
+    @getuserinfo="getUserInfo"
+    @openSetting="openSetting"
+  >
+  <view class="loading" v-show="loading">
+    <Loading></Loading>
+  </view>
+		<slot>{{title}}</slot>
+	</button>
+</template>
+
+<script>
+  import Loading from '@/pages/Components/components/Loading/Loading'
+	export default {
+    components:{
+      Loading
+    },
+		props: {
+			title: {
+				type: null,
+				default: '标题'
+			},
+      // 是否镂空
+      plain:{
+        type: Boolean,
+        default: false
+      },
+			openType: {
+				type: null,
+				default: ''
+			},
+			size: {
+				type: String,
+				default: 'm'
+			},
+			type: {
+				type: String,
+				default: 'primary'
+			},
+			disabled: {
+				type: Boolean,
+				default: false
+			},
+			customStyle: {
+        type: [String],
+				default: ''
+			},
+      // 字体颜色
+      textColor:{
+        type: [String],
+        default:'#fff'
+      },
+      // 按钮背景色
+      bgColor:{
+        type: [String],
+        default:''
+      },
+      // 显示loading
+      loading:{
+        type: Boolean,
+        default:false
+      }
+		},
+		data() {
+			return {
+        
+			};
+		},
+		computed: {
+      innerCustomStyle() {
+        let str = ''
+        if(this.textColor && !this.plain) {//镂空按钮不设置颜色
+          str += `color:${this.textColor};`
+        }
+        if(this.bgColor) {
+          if(this.bgColor.includes('linear-gradient')) { //渐变色
+            str += `background-image: ${this.bgColor};`
+          }
+          else {
+            str += `background:${this.bgColor};`
+          }
+          
+        }
+        str += this.customStyle
+        return str
+      }
+		},
+		methods: {
+			handleClick() {
+				if (!this.disabled) {
+          this.$u.debounce(()=>this.$emit('click'),500,true)
+				} else {
+					this.$emit('disabledClick');
+				}
+			},
+			getUserInfo(e) {
+				this.$emit('getuserinfo', e);
+			},
+			getPhoneNumber(e) {
+				this.$emit('getphonenumber', e);
+			},
+			openSetting(e) {
+				console.log('e', e)
+				this.$emit('openSetting', e);
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.c-button-warp {
+		box-sizing: border-box;
+		width: 100%;
+		color: #ffffff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .loading {
+      margin-right: 12rpx;
+    }
+		&:active {
+			opacity: .9;
+		}
+    // 主题
+    &.theme {
+      background: $themeColor;
+      &.plain {
+        background: transparent;
+        color: $themeColor;
+        border: 2rpx solid $themeColor;
+      }
+    }
+		// 成功
+		&.primary {
+			background: $primaryColor;
+      // background: linear-gradient(131deg,$themeColor 15%, #4ab6e8 88%);
+      &.plain {
+        background: transparent;
+        color: $primaryColor;
+        border: 2rpx solid $primaryColor;
+      }
+		}
+    // 危险
+    &.danger {
+      background: $dangerColor;
+      &.plain {
+        background: transparent;
+        color: $dangerColor;
+        border: 2rpx solid $dangerColor;
+      }
+    }
+    // 警告
+    &.warn {
+      background: $warnColor;
+      &.plain {
+        background: transparent;
+        color: $warnColor;
+        border: 2rpx solid $warnColor;
+      }
+    }
+    // 禁用
+    &.disabled {
+    	opacity: .6;
+      color: #fff; 
+    }
+    
+		// 大小
+		&.m {
+			height: 80rpx;
+			line-height: 80rpx;
+			border-radius: 44rpx;
+			font-size: 32rpx;
+		}
+
+		&.xl {
+			height: 108rpx;
+			line-height: 108rpx;
+			border-radius: 64rpx;
+			font-size: 36rpx;
+		}
+		&.s {
+			height: 60rpx;
+			line-height: 60rpx;
+			border-radius: 40rpx;
+			font-size: 26rpx;
+		}
+	}
+</style>
---
+++ pages/Components/page/Button/Button.vue
@@ -0,0 +1,126 @@
+<template>
+  <view class="Button">
+    <Title name="按钮类型"></Title>
+    <view class="btns">
+      <view class="btn">
+        <Btn  type="theme"   @click="handleClick">默认主题色</Btn>
+      </view>
+      <view class="btn">
+        <Btn  type="primary"  @click="handleClick">成功按钮</Btn>
+      </view>
+      <view class="btn">
+        <Btn  type="danger"  @click="handleClick">危险按钮</Btn>
+      </view>
+      <view class="btn">
+        <Btn  type="warn"  @click="handleClick">警告按钮</Btn>
+      </view>
+    </view>
+    <Title name="镂空按钮"></Title>
+    <view class="btns">
+      <view class="btn">
+        <Btn  type="theme" plain  @click="handleClick">镂空按钮</Btn>
+      </view>
+      <view class="btn">
+        <Btn  type="primary" plain  @click="handleClick">镂空按钮</Btn>
+      </view>
+      <view class="btn">
+        <Btn  type="danger" plain  @click="handleClick">镂空按钮</Btn>
+      </view>
+      <view class="btn">
+        <Btn  type="warn" plain  @click="handleClick">镂空按钮</Btn>
+      </view>
+    </view>
+    <Title name="显示loading"></Title>
+    <view class="btns">
+      <view class="btn">
+        <Btn  type="theme"  loading @click="handleClick">默认主题色</Btn>
+      </view>
+      <view class="btn">
+        <Btn  type="primary" loading @click="handleClick">成功按钮</Btn>
+      </view>
+      <view class="btn">
+        <Btn  type="theme" plain loading @click="handleClick">镂空按钮</Btn>
+      </view>
+      <view class="btn">
+        <Btn  type="primary" plain loading @click="handleClick">镂空按钮</Btn>
+      </view>
+    </view>
+    <Title name="禁用按钮"></Title>
+    <view class="btns">
+      <view class="btn">
+        <Btn  type="theme" disabled  @click="handleClick">镂空按钮</Btn>
+      </view>
+      <view class="btn">
+        <Btn  type="primary" disabled  @click="handleClick">镂空按钮</Btn>
+      </view>
+      <view class="btn">
+        <Btn  type="danger" disabled  @click="handleClick">镂空按钮</Btn>
+      </view>
+      <view class="btn">
+        <Btn  type="warn" disabled  @click="handleClick">镂空按钮</Btn>
+      </view>
+    </view>
+    <Title name="自定义字体颜色"></Title>
+    <view class="btns">
+      <view class="btn">
+        <Btn  type="theme" textColor="#809bee"  @click="handleClick">镂空按钮</Btn>
+      </view>
+      <view class="btn">
+        <Btn  type="primary"  textColor="#f7a4a4"  @click="handleClick">镂空按钮</Btn>
+      </view>
+    </view>
+    <Title name="自定义背景颜色"></Title>
+    <view class="btns">
+      <view class="btn">
+        <Btn  type="theme" bgColor="linear-gradient(160deg, #0093E9 0%,#8EC5FC 46%, #80D0C7 100%)"  @click="handleClick">渐变色按钮</Btn>
+      </view>
+      <view class="btn">
+        <Btn  type="theme" bgColor="linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)"  @click="handleClick">渐变色按钮</Btn>
+      </view>
+      <view class="btn">
+        <Btn  type="primary"  bgColor="linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%)"  @click="handleClick">渐变色按钮</Btn>
+      </view>
+      <view class="btn">
+        <Btn  type="primary"  bgColor="#0ab99c"  @click="handleClick">青绿色按钮</Btn>
+      </view>
+    </view>
+    <Title name="自定义大小"></Title>
+    <view class="btns">
+      <view class="btn">
+        <Btn  type="primary" size="xl" customStyle="width:324rpx;" @click="handleClick">超大尺寸</Btn>
+      </view>
+      <view class="btn">
+        <Btn  type="danger" size="m"  @click="handleClick">普通尺寸</Btn>
+      </view>
+      <view class="btn">
+        <Btn  type="warn" size="s" customStyle="width:200rpx;" @click="handleClick">小型尺寸</Btn>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script setup>
+  import Btn from '@/pages/Components/components/Button/Button.vue'
+  import Title from '@/components/MainUI/Title.vue'
+  import Item from '@/components/MainUI/Item.vue'
+  import {ref} from 'vue'
+  
+  let customStyle = ref("width:224rpx;")
+</script>
+
+<style lang="scss" scoped>
+  .Button{
+    padding: 24rpx;
+    .btns {
+      display: grid;
+      grid-template-columns:repeat(2,1fr);
+      grid-row-gap: 44rpx;
+      margin-bottom: 54rpx;
+      .btn {
+        width:250rpx;
+        // margin-bottom: 24rpx;
+      }
+    }
+    
+  }
+</style>
